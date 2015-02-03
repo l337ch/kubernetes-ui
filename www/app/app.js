88a74774a6ec9054f473fbe1d9557b9998a5db04
rrmckinley <mckinley1411@gmail.com>
2015-02-02 23:51:43 -0800

    Request services and prepare to populate UI.

---
+++ www/app/app.js
@@ -0,0 +1,73 @@
+(function(){
+'use strict';
+
+  angular
+      .module('krakenApp', ['ngMaterial', 'visualizers'])
+      .controller('AppCtrl', ['$scope', 'visualizersService', '$mdSidenav', '$mdBottomSheet', '$log', VisualizerAppController ])
+      .config(function($mdThemingProvider) {
+
+        // Place theme overrides here.
+        // Example:
+        // $mdThemingProvider.theme('default')
+        //   .primaryColor('red')
+        //   .accentColor('red');
+
+      });
+
+  /**
+   * Main Controller for the Kraken App
+   * @param $scope
+   * @param $mdSidenav
+   * @param visualizersService
+   * @constructor
+   */
+  function VisualizerAppController($scope, visualizersService, $mdSidenav, $mdBottomSheet, $log ) {
+    var allVisualizers = [ ];
+
+    $scope.selected      = null;
+    $scope.visualizers       = allVisualizers;
+    $scope.selectVisualizer  = selectVisualizer;
+    $scope.toggleSidenav = toggleSideNav;
+
+    loadVisualizers();
+
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    /**
+     * Load visualizers
+     * @param menuId
+     *
+     */
+    function loadVisualizers() {
+      visualizersService
+        .loadAll()
+        .then( function( visualizers ) {
+          allVisualizers = visualizers;
+
+          $scope.visualizers = [].concat(visualizers);
+          $scope.selected = visualizers[0];
+        });
+    }
+
+    /**
+     * Toggle sideNav
+     * @param menuId
+     */
+    function toggleSideNav( name ) {
+      $mdSidenav(name).toggle();
+    }
+
+    /**
+     * Select current visualizer
+     * @param menuId
+     */
+    function selectVisualizer ( visualizer ) {
+        $scope.selected = angular.isNumber(visualizer) ? $scope.visualizers[visualizer] : visualizer;
+        $scope.toggleSidenav('left');
+    }
+
+  }
+
+})();
