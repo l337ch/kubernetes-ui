14e06830b733ea62f94b8175b899752761018950
rrmckinley <mckinley1411@gmail.com>
2015-02-05 08:08:00 -0800

    Add angular boilerplate to mock services to simulate async

--- www/app/assets/js/replicationControllersMock.js
+++ www/app/assets/js/replicationControllersMock.js
@@ -1,8 +1,33 @@
-{
-  "kind": "ReplicationControllerList",
-  "creationTimestamp": null,
-  "selfLink": "/api/v1beta1/replicationControllers",
-  "resourceVersion": 166552,
-  "apiVersion": "v1beta1",
-  "items": []
-}
+(function(){
+  'use strict';
+
+  angular.module('replicationControllers', [ ])
+         .service('replicationControlleService', ReplicationControllerDataService);
+
+  /**
+   * Replication Controller DataService
+   * Mock async data service.
+   *
+   * @returns {{loadAll: Function}}
+   * @constructor
+   */
+  function ReplicationControllerDataService($q){
+    var replicationControllers = {
+      "kind": "ReplicationControllerList",
+      "creationTimestamp": null,
+      "selfLink": "/api/v1beta1/replicationControllers",
+      "resourceVersion": 166552,
+      "apiVersion": "v1beta1",
+      "items": []
+    };
+
+    // Uses promises
+    return {
+      loadAll : function() {
+        // Simulate async call
+        return $q.when(replicationControllers);
+      }
+    };
+  }
+
+})();
