09dab55f5e4e1cb726418c1b17094571effa0ea1
rrmckinley <mckinley1411@gmail.com>
2015-02-05 07:33:52 -0800

    Add some actual Kubernetes API responses as mock data services

---
+++ www/app/assets/js/replicationControllersMock.js
@@ -0,0 +1,8 @@
+{
+  "kind": "ReplicationControllerList",
+  "creationTimestamp": null,
+  "selfLink": "/api/v1beta1/replicationControllers",
+  "resourceVersion": 166552,
+  "apiVersion": "v1beta1",
+  "items": []
+}
