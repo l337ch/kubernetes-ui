fe1e3a9c8d5a6fbf74c917d8135010dceb56c6dc
rrmckinley <mckinley1411@gmail.com>
2015-02-05 01:41:07 -0800

    Stub out pages and angular controllers for each visualizer

--- www/app/assets/js/app.js
+++ www/app/assets/js/app.js
@@ -3,7 +3,11 @@ var app = angular.module('krakenApp', ['ngRoute','ngMaterial']);
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when("/", {templateUrl: "/views/partials/home.html", controller: "PageCtrl"})
+    .when("/clusters", {templateUrl: "/pages/clusters.html", controller: "PageCtrl"})
     .when("/pods", {templateUrl: "/pages/pods.html", controller: "PageCtrl"})
+    .when("/replication", {templateUrl: "/pages/replication.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "/pages/services.html", controller: "PageCtrl"})
+    .when("/labels", {templateUrl: "/pages/labels.html", controller: "PageCtrl"})
     .when("/404", {templateUrl: "/views/partials/404.html", controller: "PageCtrl"})
     // else 404
     .otherwise({
@@ -35,7 +39,84 @@ app.directive('includeReplace', function () {
     };
 });
 
+/**=========================================================
+ * Module: Clusters
+ * Visualizer for clusters
+ =========================================================*/
+
+app.controller('ClusterCtrl', ['$scope', '$interval',
+    function($scope, $interval) {
+      $scope.mode = 'query';
+      $scope.determinateValue = 30;
+      $interval(function() {
+        $scope.determinateValue += 1;
+        if ($scope.determinateValue > 100) {
+          $scope.determinateValue = 30;
+        }
+      }, 100, 0, true);
+    }
+  ]);
+
+/**=========================================================
+ * Module: Pods
+ * Visualizer for pods
+ =========================================================*/
+
 app.controller('PodCtrl', ['$scope', '$interval',
+    function($scope, $interval) {
+      $scope.mode = 'query';
+      $scope.determinateValue = 30;
+      $interval(function() {
+        $scope.determinateValue += 1;
+        if ($scope.determinateValue > 100) {
+          $scope.determinateValue = 30;
+        }
+      }, 100, 0, true);
+    }
+  ]);
+
+/**=========================================================
+ * Module: Replication
+ * Visualizer for replication controllers
+ =========================================================*/
+
+app.controller('ReplicationCtrl', ['$scope', '$interval',
+    function($scope, $interval) {
+      $scope.mode = 'query';
+      $scope.determinateValue = 30;
+      $interval(function() {
+        $scope.determinateValue += 1;
+        if ($scope.determinateValue > 100) {
+          $scope.determinateValue = 30;
+        }
+      }, 100, 0, true);
+    }
+  ]);
+
+/**=========================================================
+ * Module: Services
+ * Visualizer for services
+ =========================================================*/
+
+app.controller('ServicesCtrl', ['$scope', '$interval',
+    function($scope, $interval) {
+      $scope.mode = 'query';
+      $scope.determinateValue = 30;
+      $interval(function() {
+        $scope.determinateValue += 1;
+        if ($scope.determinateValue > 100) {
+          $scope.determinateValue = 30;
+        }
+      }, 100, 0, true);
+    }
+  ]);
+
+/**=========================================================
+ * Module: Labels
+ * Visualizer for labels
+ =========================================================*/
+
+app.controller('LabelCtrl', ['$scope', '$interval',
     function($scope, $interval) {
       $scope.mode = 'query';
       $scope.determinateValue = 30;
