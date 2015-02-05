327267c2e32e25892c7431bcd65fec9539c13352
Patrick Reilly <patrick@kismatic.io>
2015-02-05 13:54:31 -0800

    remove console.log

--- www/app/assets/js/app.js
+++ www/app/assets/js/app.js
@@ -35,7 +35,6 @@ app.controller('AppCtrl', function( $scope ) {
   });
 
 app.controller('PageCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  console.log("loading page controller.");
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
