049f7e094526d0b0615d33dff19b47facde8bcba
Patrick Reilly <patrick@kismatic.io>
2015-02-05 12:55:08 -0800

    basic tabs example

--- www/app/assets/js/app.js
+++ www/app/assets/js/app.js
@@ -16,6 +16,24 @@ app.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
+angular.module('whiteframeBasicUsage', ['ngMaterial']);
+
+app.controller('AppCtrl', function( $scope ) {
+    $scope.data = {
+      selectedIndex : 0,
+      secondLocked : true,
+      firstLabel : "Dashboard",
+      secondLabel : "Graph",
+      thirdLabel : "Other"
+    };
+    $scope.next = function() {
+      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+    };
+    $scope.previous = function() {
+      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+    };
+  });
+
 app.controller('PageCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   console.log("loading page controller.");
   $scope.toggleSidenav = function(menuId) {
