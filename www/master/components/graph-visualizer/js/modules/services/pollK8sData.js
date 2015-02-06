0119909243390039097ea9430a57b56661d07741
Xin Zhang <xinzh@google.com>
2015-02-06 16:48:09 -0500

    Add an AngularJS service that periodically polls K8s data from a backend server.

---
+++ www/master/components/graph-visualizer/js/modules/services/pollK8sData.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  angular.module('pollK8sData', []);
+         .service('pollK8sDataService', PollK8sDataService);
+
+  var PollK8sDataService = function($http, $timeout) {
+    var k8sdatamodel = undefined;
+    var pollingError = 0;
+
+    var startPolling = function() {
+      // TODO: maybe display an error to the end user.
+      if (pollingError > 3) {
+        console.log('Have ' + pollingError + ' consecutive polling errors.');
+      }
+
+      // TODO: Pass in the real URL
+      $http.get('http://turing-glider-846.appspot.com/graph').
+        success(function(data, status, headers, config) {
+        if (data) {
+          k8sdatamodel = data;
+          pollingError = 0;
+        } else {
+          pollingError++;
+        }
+
+        $timeout(startPolling, 1000); }).
+        error(function(data, status, headers, config) {
+          pollingError++;
+          $timeout(startPolling, 1000);
+        });
+    };
+
+    startPolling();
+
+    return {
+      getSnapshot : function() {
+        return k8sdatamodel;
+      }
+    };
+  };
+
+})
