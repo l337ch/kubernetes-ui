c98a84976953cd9a5f1840976100b52ad040bc6a
Patrick Reilly <patrick@kismatic.io>
2015-02-10 14:30:25 -0800

    make getConstant reusable

--- www/master/js/app.config.js
+++ www/master/js/app.config.js
@@ -9,14 +9,37 @@ app.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
-app.config(function(k8sApiProvider, ENV){
-
-  var getConstant = function(constantName) {
-    if (ENV[constantName]) {
-      return ENV[constantName];
+app.provider('configurationService', ['ENV', function(ENV) {
+  var env = ENV; // allow ability to override the real ENV
+  this.getConstant = function(constantName) {
+    if (env[constantName]) {
+      return env[constantName];
     }
-    return false;
+      return false;
+    };
+
+  this.getConstantOrElse = function(constantName, alternateValue) {
+    if (env[constantName]) {
+      return env[constantName];
+    }
+      return alternateValue;
+    };
+
+  this.setEnv = function(value) {
+    env = value;
   };
 
-  k8sApiProvider.setUrlBase(getConstant('k8sApiServer'));
+  this.$get = function(env){
+    var configMethods = {};
+    var toBeReturnedEnv = this.env || env;
+    configMethods.getConstant = function (constantName) {
+      return toBeReturnedEnv[constantName];
+    };
+
+    return configMethods;
+  }
+}]);
+
+app.config(function(k8sApiProvider,configurationServiceProvider, ENV){
+  k8sApiProvider.setUrlBase(configurationServiceProvider.getConstant('k8sApiServer'));
 });
