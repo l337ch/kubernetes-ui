870d44207492319b52244ac67b362ce33d2fac1f
BC Broussard <bc.broussard@flyingcranes.com>
2015-02-24 16:21:07 -0800

    change group function and minor fixes

--- www/master/js/app.init.js
+++ www/master/js/app.init.js
@@ -5,7 +5,7 @@
 // ****************************
 // -----------------------------------
 
-var app = angular.module('krakenApp', ['ngRoute','ngMaterial', 'ngLodash', 'krakenApp.config'].concat(componentNamespaces))
+var app = angular.module('krakenApp', ['ngRoute','ngMaterial', 'ngLodash', 'krakenApp.config', 'angular.filter'].concat(componentNamespaces))
 
 .factory('menu', [
   '$location',
