7693f6593774a15d26aa171449084891ded7bcf5
xinzh-mac <xinzh@google.com>
2015-03-06 11:25:40 -0500

    add basic e2e smoke tests for graph

--- www/master/protractor/components/graph/smoke.spec.js
+++ www/master/protractor/components/graph/smoke.spec.js
@@ -1,6 +1,28 @@
-describe('Kubernetes UI chrome', function() {
-  it('should have the right title', function() {
+describe('Kubernetes UI Graph', function() {
+  it('should have all the expected components loaded', function() {
     browser.get('http://localhost:8000');
     expect(browser.getTitle()).toEqual('Kubernetes UI');
+
+    // Navigate to the graph page
+    var graphTab = element(by.id('tab_002'));
+    expect(graphTab).toBeDefined();
+    graphTab.click();
+    expect(browser.getLocationAbsUrl())
+        .toBe('/graph');
+
+    // Verify if the control action icons have been loaded.
+    var expandCollapose = element(by.id('ExpandCollapse'));
+    expect(expandCollapose).toBeDefined();
+    var toggleSelect = element(by.id('ToggleSelect'));
+    expect(toggleSelect).toBeDefined();
+    var toggleSource = element(by.id('ToggleSource'));
+    expect(toggleSource).toBeDefined();
+    var pollOnce = element(by.id('PollOnce'));
+    expect(pollOnce).toBeDefined();
+
+    // Use mock data to ease testing.
+    toggleSource.click();
+    // Just pull once to get a stable graph.
+    pollOnce.click();
   });
 });
